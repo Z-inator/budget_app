33054cda85df4ca4797afabe3fb5e18655f1808d
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-12-02 15:35:23 -0600

    Added Firebase Core and connection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'package:budget_app/components/budget_bar.dart';
 import 'package:budget_app/components/item_list.dart';
 import 'package:budget_app/components/new_item_button.dart';
 import 'package:budget_app/models/spending_item.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Budget App',
       theme: buildTheme(ThemeData.light()),
-      home: HomePage(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorPage(errorMessage: snapshot.error.toString());
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return HomePage();
+          }
+          return LoadingPage();
+        },
+      ),
     );
   }
 }
@@ -28,16 +48,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: MonthBudgetBar(),
-        body: ItemList(items: items),
-        floatingActionButton: NewItemButton(),
-      )
-    );
+        child: Scaffold(
+      appBar: MonthBudgetBar(),
+      body: ItemList(items: items),
+      floatingActionButton: NewItemButton(),
+    ));
   }
 }
 
+class ErrorPage extends StatelessWidget {
+  final String errorMessage;
+  const ErrorPage({
+    Key? key,
+    required this.errorMessage,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Text(errorMessage),
+      ),
+    ));
+  }
+}
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(body: CircularProgressIndicator(),));
+  }
+}
 
 ThemeData buildTheme(ThemeData base) {
   return base.copyWith();
