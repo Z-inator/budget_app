8e13d403aef351d5463f207ca2564a0b0d0bc76d
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-12-14 09:32:37 -0600

    Built preliminary item Expansion Tile

--- lib/components/item_list.dart
+++ lib/components/item_list.dart
@@ -1,7 +1,8 @@
+import 'package:budget_app/components/new_item_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:intl/intl.dart';
 import 'package:budget_app/main.dart';
 import 'package:budget_app/models/spending_item.dart';
 import 'package:budget_app/services/firestore.dart';
@@ -12,14 +13,14 @@ class ItemList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<SpendingItem> items = Provider.of<Database>(context).getOrderedSpendingItems();
+    List<SpendingItem> items =
+        Provider.of<Database>(context).getOrderedSpendingItems();
     return ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) => Item(
-                    item: items[index],
-                  ));
+        itemCount: items.length,
+        itemBuilder: (context, index) => Item(
+              item: items[index],
+            ));
   }
-
 
   // @override
   // Widget build(BuildContext context) {
@@ -52,6 +53,30 @@ class Item extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: ExpansionTile(
+        leading: Row(
+          children: [
+            Icon(Icons.attach_money_rounded, color: Colors.greenAccent),
+            Text(NumberFormat('#,##0.00', 'en_US').format(item.cost))
+          ],
+        ),
+        title: Text(item.name),
+        subtitle: Text(item.vendor),
+        children: [
+          ListTile(
+            title: Text(DateFormat.yMMMMd('en_US').format(item.createDate)),
+            subtitle: Text(
+              item.description,
+            ),
+            trailing: IconButton(
+                onPressed: () => showEditSheet(
+                    context: context, isNew: false, spendingItem: item),
+                icon: Icon(Icons.edit_rounded)),
+          )
+        ],
+      ),
+    );
   }
 }
