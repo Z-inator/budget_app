64318b3824c7778e858e8eed1b740b26d4e56870
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-12-20 10:54:15 -0600

    Added Google Sign in

--- lib/services/authentification.dart
+++ lib/services/authentification.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -9,5 +11,40 @@ class AuthService {
 
   Stream<User?> get onAuthChanged {
     return auth.idTokenChanges();
+  }
+
+  Future<String> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
+    try {
+      UserCredential userCredential =
+          await auth.signInWithCredential(credential);
+      User? user = userCredential.user;
+      if (userCredential.additionalUserInfo!.isNewUser && user != null) {
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .set({'displayName': user.displayName});
+      }
+    } on FirebaseAuthException catch (error) {
+      switch (error.code) {
+        case 'account-exists-with-different-credential':
+          return 'The account already exists using a different register method';
+        case 'invalid-credential':
+          return 'Invalid credentials. Try again.';
+        default:
+          return 'Error using Google Sign In. Try again.';
+      }
+    } catch (error) {
+      return 'Error using Google Sign In. Try again.';
+    }
+    return 'Success signing in with Google';
+  }
+
+  Future signOut() async {
+    return auth.signOut();
   }
 }
