d3103a604a1d6345b3f4369f94c44909d2119eeb
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-12-21 15:36:03 -0600

    Built Calculations for spending items.

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,3 +1,4 @@
+import 'package:budget_app/models/document_user.dart';
 import 'package:budget_app/models/spending_item.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,6 +7,9 @@ import 'package:flutter/cupertino.dart';
 class Database with ChangeNotifier {
   final User user;
   final CollectionReference<SpendingItem> spendingItemReference;
+  final DocumentReference userDocumentReference;
+  late DocumentUser documentUser;
+  late List<SpendingItem> items;
 
   Database({required this.user})
       : spendingItemReference = FirebaseFirestore.instance
@@ -15,7 +19,23 @@ class Database with ChangeNotifier {
             .withConverter<SpendingItem>(
                 fromFirestore: (snapshot, _) =>
                     SpendingItem.fromMap(snapshot.data()!, snapshot.reference),
-                toFirestore: (spendingItem, _) => spendingItem.toMap());
+                toFirestore: (spendingItem, _) => spendingItem.toMap()),
+        userDocumentReference = FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .withConverter<DocumentUser>(
+                fromFirestore: (snapshot, _) =>
+                    DocumentUser.fromMap(snapshot.data()!),
+                toFirestore: (userDocument, _) => userDocument.toMap()) {
+    getDocumentUserDetails();
+    getOrderedItems();
+  }
+
+  Future<void> getDocumentUserDetails() {
+    return userDocumentReference.get().then((DocumentSnapshot snapshot) {
+      documentUser = snapshot.data()! as DocumentUser;
+    });
+  }
 
   Stream<QuerySnapshot<SpendingItem>> spendingItemStream() {
     return spendingItemReference
@@ -23,12 +43,67 @@ class Database with ChangeNotifier {
         .snapshots();
   }
 
-  List<SpendingItem> getOrderedSpendingItems() {
-    List<SpendingItem> items = [];
+  List<SpendingItem> getOrderedItems() {
     spendingItemStream().first.then((value) {
       value.docs.map((documentSnapshot) => items.add(documentSnapshot.data()));
     });
     return items;
+  }
+
+  List<SpendingItem> getSpentItems() {
+    return items.where((item) => item.cost > 0).toList();
+  }
+
+  List<SpendingItem> getSurplusItems() {
+    return items.where((item) => item.cost < 0).toList();
+  }
+
+  double getTotalSurplus() {
+    double cost = 0;
+    getSurplusItems().forEach((element) {
+      cost += element.cost;
+    });
+    return cost;
+  }
+
+  int getTotalMonths() {
+    DateTime createDate = documentUser.createDate;
+    DateTime now = DateTime.now();
+    return now.month +
+        (12 - createDate.month) +
+        (12 * (now.year - createDate.year));
+  }
+
+  double getTotalSpendingAdded() {
+    return getTotalSurplus() + (getTotalMonths() * 100.0);
+  }
+
+  double getSurplusAvailable() {
+    return getTotalSpendingAdded() - getTotalSpent();
+  }
+
+  double getTotalSpent() {
+    double cost = 0;
+    getSpentItems().forEach((element) {
+      cost += element.cost;
+    });
+    return cost;
+  }
+
+  List<SpendingItem> currentMonthSpentItems() {
+    return getSpentItems()
+        .where((element) =>
+            element.createDate.year == DateTime.now().year &&
+            element.createDate.month == DateTime.now().month)
+        .toList();
+  }
+
+  double currentMonthAvailbleSpending() {
+    double spent = 0;
+    currentMonthSpentItems().forEach((element) {
+      spent += element.cost;
+    });
+    return spent;
   }
 
   Future<void> addSpendingItem(SpendingItem item) {
@@ -52,5 +127,12 @@ class Database with ChangeNotifier {
         .delete()
         .then((value) => print("Spending Item Deleted"))
         .catchError((error) => print('Failed to delete Spending Item: $error'));
+  }
+
+  Future updateUserDocument(Map<String, dynamic> data) {
+    return userDocumentReference
+        .update(data)
+        .then((value) => print('User document updated'))
+        .catchError((error) => print('Failed to update user document: $error'));
   }
 }
